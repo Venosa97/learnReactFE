d6bc83fbfad8d8c42d1250c8ec57c9f3aa9d79b2
costi <costivn@gmail.com>
2024-05-15 10:35:57 +0300

    Demo app

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,27 @@
+import Post from "./Post";
+import styles from "./PostsList.module.css";
+import { useLoaderData } from "react-router-dom";
+
+function PostsList() {
+  const posts = useLoaderData();
+
+  return (
+    <>
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={post.id} id={post.id} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yer.</h2>
+          <p>Start adding some!</p>{" "}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostsList;
